d9a0df55f43d18050afb517a8a5781035a105c74
Diogo Krub de Almeida <diogokalmeida@gmail.com>
2023-04-16 17:11:35 -0700

    icon and day repetition fixed.

--- assets/js/app.js
+++ assets/js/app.js
@@ -81,104 +81,110 @@ const getHoursDayWeather = (lat, lon) => {
         .then((data) => { return data.json() })
         .then((data) => {
 
-//////////////////////////////////////Daily Weather//////////////////////////////////////
+            //////////////////////////////////////Daily Weather//////////////////////////////////////
 
-    const obj = {};
-    const dailyForecast = {};
+            const obj = {};
+            const dailyForecast = {};
 
-    let monthArray=[
-        "January","February","March","April","May","June",
-        "July","August","September","Octobar","November","December"
-    ];
+            let monthArray = [
+                "January", "February", "March", "April", "May", "June",
+                "July", "August", "September", "Octobar", "November", "December"
+            ];
 
-    let time = "";
-    
-    for (let i = 0; i < data.list.length; i++) {
-        const dateTime = new Date(data.list[i].dt * 1000);
-        const month = dateTime.getMonth();
-        const date = dateTime.getDate();
-        time = monthArray[month] + ", " + date;
-        if (!obj[time]) {
-        obj[time] = [data.list[i]];
-        } else {
-        obj[time].push(data.list[i]);
-        }
-    }
-   
-    for (const day in obj) {
-        // console.log('--------Day---------', obj[day]);
-        let maxTempDay;
-        let minTempDay;
-        let sumAverageTemp;
-        maxTempDay = obj[day].reduce((acc, curr) => {
-        return acc > curr.main.temp_max ? acc : curr.main.temp_max;
-        }, 0);
-        // console.log("maxTempDay", Math.floor(maxTempDay));
-        minTempDay = obj[day].reduce((acc, curr) => {
-        return acc < curr.main.temp_min ? acc : curr.main.temp_min;
-        });
-        // console.log("minTempDay", Math.floor(minTempDay));
-        sumAverageTemp = obj[day].reduce((acc, curr) => {
-        return acc + curr.main.temp;
-        }, 0);
-        let averageTemp = sumAverageTemp / obj[day].length;
-        console.log("Average Temp for the day", Math.floor(averageTemp));
-        let loggedIcon = obj[day].reduce((acc, curr) => {
-        return acc > Number(curr.weather[0].icon.slice(0, 2))
-            ? acc
-            : Number(curr.weather[0].icon.slice(0, 2));
-        }, 0);
+            let time = "";
 
-        if (loggedIcon < 10) {
-            loggedIcon = "0" + loggedIcon;
-        }
-        let srcIcon = loggedIcon + "d";
+            for (let i = 0; i < data.list.length; i++) {
+                const dateTime = new Date(data.list[i].dt * 1000);
+                const month = dateTime.getMonth();
+                const date = dateTime.getDate();
+                time = monthArray[month] + ", " + date;
+                if (!obj[time]) {
+                    obj[time] = [data.list[i]];
+                } else {
+                    obj[time].push(data.list[i]);
+                }
+            }
 
-        console.log('Icon for the day',loggedIcon);
-        dailyForecast[day] = {
-        Temperature: Math.floor(averageTemp),
-        MaxTemperature: Math.floor(maxTempDay),
-        MinTemperature: Math.floor(minTempDay),
-        icon: srcIcon,
-        };
-    }
+            for (const day in obj) {
+                // console.log('--------Day---------', obj[day]);
+                let maxTempDay;
+                let minTempDay;
+                let sumAverageTemp;
+                maxTempDay = obj[day].reduce((acc, curr) => {
+                    return acc > curr.main.temp_max ? acc : curr.main.temp_max;
+                }, 0);
+                // console.log("maxTempDay", Math.floor(maxTempDay));
+                minTempDay = obj[day].reduce((acc, curr) => {
+                    return acc < curr.main.temp_min ? acc : curr.main.temp_min;
+                }, +Infinity);
+                // console.log("minTempDay", Math.floor(minTempDay));
+                sumAverageTemp = obj[day].reduce((acc, curr) => {
+                    return acc + curr.main.temp;
+                }, 0);
+                let averageTemp = sumAverageTemp / obj[day].length;
+                console.log("Average Temp for the day", Math.floor(averageTemp));
+                let loggedIcon = obj[day].reduce((acc, curr) => {
+                    return acc > Number(curr.weather[0].icon.slice(0, 2))
+                        ? acc
+                        : Number(curr.weather[0].icon.slice(0, 2));
+                }, 0);
 
-    const dailyList=document.querySelector(".daily ul")
-    let i=1;
-    for(let key in dailyForecast){
-        if(i<=5){
-            let li=
-            '<li class="daily-items position'+i+'"><span class="bold">'+key+
-            '</span><img class="icon'+key.split(", ")[1]+'"><span class="bold">'
-            +dailyForecast[key].Temperature +'&deg;C</span><div><span>Max:&nbsp;'
-            +dailyForecast[key].MaxTemperature+'&deg;C</span><span>Min:&nbsp;'
-            +dailyForecast[key].MinTemperature+'&deg;C</span></div></li>'
-            dailyList.insertAdjacentHTML("beforeend", li);
-            const getImg = document.querySelector(".icon" + key.split(", ")[1]);
-            //getImg.src = `http://openweathermap.org/img/wn/${dailyForecast[key].srcIcon}@2x.png`;
-            getImg.src = `http://openweathermap.org/img/wn/10d@2x.png`;
-        }
-        i+=1;
-    }
+                if (loggedIcon < 10) {
+                    loggedIcon = "0" + loggedIcon;
+                }
+                let srcIcon = loggedIcon + "d";
 
-/////////////////////////////////////////////////////////////////////////////////
+                console.log('Icon for the day', loggedIcon);
+                dailyForecast[day] = {
+                    Temperature: Math.floor(averageTemp),
+                    MaxTemperature: Math.floor(maxTempDay),
+                    MinTemperature: Math.floor(minTempDay),
+                    icon: srcIcon,
+                };
+            }
+
+            const dailyList = document.querySelector(".daily ul")
+            let i = 1;
+            while (dailyList.firstChild) {
+                dailyList.removeChild(dailyList.firstChild);
+            }
+
+            for (let key in dailyForecast) {
+                if (i <= 5) {
+                    let li =
+                        '<li class="daily-items position' + i + '"><span class="bold">' + key +
+                        '</span><img class="icon' + key.split(", ")[1] + '"><span class="bold">'
+                        + dailyForecast[key].Temperature + '&deg;C</span><div><span>Max:&nbsp;'
+                        + dailyForecast[key].MaxTemperature + '&deg;C</span><span>Min:&nbsp;'
+                        + dailyForecast[key].MinTemperature + '&deg;C</span></div></li>'
+                    dailyList.insertAdjacentHTML("beforeend", li);
+                    const getImg = document.querySelector(".icon" + key.split(", ")[1]);
+
+                    console.log("dailyForecast[key].srcIcon", dailyForecast[key].icon);
+                    getImg.src = `http://openweathermap.org/img/wn/${dailyForecast[key].icon}@2x.png`;
+
+                }
+                i += 1;
+            }
+
+            /////////////////////////////////////////////////////////////////////////////////
 
             const hourlyData = (value) => {
-        
-                value.style.background='#7eb1eb';
+
+                value.style.background = '#7eb1eb';
                 const number = parseInt(value.classList[1].split("position")[1]);
                 const dailyItems = window.getComputedStyle(document.querySelector(".daily"));
-                
+
                 const width = parseInt(dailyItems.getPropertyValue('width'));
                 const padding = parseInt(dailyItems.getPropertyValue('padding-left'));
                 const arrowWidth = parseInt(document.querySelector(".upArrow svg").getAttribute("width"))
-                document.querySelector(".upArrow").style.left=(width/5)*(number-1/2)+padding-arrowWidth/2+"px"
+                document.querySelector(".upArrow").style.left = (width / 5) * (number - 1 / 2) + padding - arrowWidth / 2 + "px"
 
                 const height = parseInt(dailyItems.getPropertyValue('height'));
                 const padding_top = parseInt(dailyItems.getPropertyValue('padding-top'));
                 const arrowHeight = parseInt(document.querySelector(".rightArrow svg").getAttribute("height"))
-                document.querySelector(".rightArrow").style.top=((height-padding_top*2)/5)*(number-1/2)+padding_top-arrowHeight/2+"px"
-                    
+                document.querySelector(".rightArrow").style.top = ((height - padding_top * 2) / 5) * (number - 1 / 2) + padding_top - arrowHeight / 2 + "px"
+
                 const dateConvert_First = value.children[0].innerText.split(", ")[1];
                 data.list.forEach((value2, index) => {
                     const dateTime = new Date(value2.dt * 1000);
@@ -186,7 +192,7 @@ const getHoursDayWeather = (lat, lon) => {
                     const date = dateTime.getDate();
 
                     if (date == dateConvert_First) {
-                        
+
                         const month = dateTime.getMonth() + 1;
                         const date = dateTime.getDate();
                         const hours = dateTime.getHours();
@@ -201,13 +207,13 @@ const getHoursDayWeather = (lat, lon) => {
 
                         const time = '<span class="time">' + convertTo12h + '</span>';
                         const icon = '<img class="weatherIcon' + index + '"/>';
-                        const weather = '<span>'+data.list[index]["weather"][0]["main"]+'</span>';
-                        const temp = '<span>'+Math.round(value2["main"]["temp"]) + "&deg;C</span>";
+                        const weather = '<span>' + data.list[index]["weather"][0]["main"] + '</span>';
+                        const temp = '<span>' + Math.round(value2["main"]["temp"]) + "&deg;C</span>";
                         const li = '<li><div>' + time + icon + weather + temp + '</div></li>';
-                        
+
                         document.querySelector(".hourly .showDate").innerText = value.children[0].innerText;
                         document.querySelector(".hourly ul").insertAdjacentHTML("beforeend", li);
-                        
+
                         const iconImg = value2["weather"][0]["icon"];
                         const getImg = document.querySelector(".weatherIcon" + index);
                         getImg.src = `http://openweathermap.org/img/wn/${iconImg}@2x.png`;
@@ -223,7 +229,7 @@ const getHoursDayWeather = (lat, lon) => {
 
                 value.addEventListener("click", () => {
 
-                    dailyData.forEach((value) => {value.style.background="#A8C9EF"})
+                    dailyData.forEach((value) => { value.style.background = "#A8C9EF" })
 
                     const hourlyLi = document.querySelectorAll(".hourly li");
                     hourlyLi.forEach((vl) => {
@@ -231,10 +237,10 @@ const getHoursDayWeather = (lat, lon) => {
                     });
 
                     hourlyData(value);
-                        
+
                 })//addEventListner
             })//forEach
-            
+
         })
         .catch((error) => {
             alert(`Failed to get weather data: ${error}. Please try again later.`);
